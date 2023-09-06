861155580489c6ec7affa6037268538ff0344c56
Aryaan14 <143996054+Aryaan14@users.noreply.github.com>
2023-09-06 14:37:42 +0400

    Add files via upload

---
+++ pythonfirst/test.py
@@ -0,0 +1,11 @@
+def hello():
+    name = input("Name: ")
+    birth_month = input("Birth Month: ")
+    day = input("Day: ")
+    year = input("Year: ")
+    print(name,"your birthday is on",birth_month,day,",",year,)
+
+
+hello()
+hello()
+hello()
